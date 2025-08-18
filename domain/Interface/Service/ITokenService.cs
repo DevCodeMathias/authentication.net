1b489d73daf88e9f700c00c56e0119d9e036909d
DevCodeMathias <lauramathias03@gmail.com>
2025-08-18 19:17:02 -0300

    add worker

--- domain/Interface/Service/ITokenService.cs
+++ domain/Interface/Service/ITokenService.cs
@@ -5,7 +5,6 @@ namespace API_AUTENTICATION.domain.Interfaces.Service
     public interface ITokenService
     {
         string GenerateToken(string email);
-
         ClaimsPrincipal ValidateToken(string token);
     }
 }
